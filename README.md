f373cd9a47321e5238c74fbde58d40f25c24efb1
Anton Guda <atu@nmetau.edu.ua>
2018-08-16 19:25:45 +0300

    Add readme, license

---
+++ README.md
@@ -0,0 +1,4 @@
+imghand - small Qt-based application for images fractal analisys.
+
+Requirements: Qt5, opencv, gsl.
+
--- mk_arch_head.sh
+++ mk_arch_head.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dstt=$( date '+%Y%m%d' )
-proj='imghead'
+proj='imghand'
 git archive --format=tar.gz --prefix=${proj}-${dstt}/ -o ../${proj}-${dstt}.tar.gz HEAD
 
 
